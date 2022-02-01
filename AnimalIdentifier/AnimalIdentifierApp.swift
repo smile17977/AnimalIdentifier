87da88b762bada7d0c9f993c2fc317a689409f89
smile17977 <65645958+smile17977@users.noreply.github.com>
2022-02-01 22:15:41 +0300

    Initial Commit

---
+++ AnimalIdentifier/AnimalIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalIdentifierApp.swift
+//  AnimalIdentifier
+//
+//  Created by Kirill Presnyakov on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
